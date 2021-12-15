c985d225a7284450f0b15e039a31e986d6f3cc62
thevic16 <20171408@ce.pucmm.edu.do>
2021-12-15 11:25:19 -0400

    update changes

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
+#!/usr/bin/python3.7
+
 # Import threading module.
 import threading
 #Module for API-REST-Request----------------------------------------------------------------
-# Import picamara from PiCamara module.
-from picamera import PiCamera
-# Importing time.
-import time
-# Importing base64 (encoding and decoding functionality).
-import base64
-# Import requests to make HTTP requests.
-import requests
-import json
-# Import date class from datetime module.
-from datetime import date
-# Import the entire datetime module.
-from datetime import datetime
-#Import geocoder module.
-import geocoder
-#Import modulo to ensure internet conection
-import urllib.request
-# Module that helps to send SMS message.
-from curses import ascii
-# importing module of wheelchair-gps
-import wheelchair_gps
-# importing module of wheelchair-serial_ports
-import wheelchair_serial_ports
 from wheelchair_api_rest_request_thread import api_rest_request_thread
 from wheelchair_gprs import test_GPRS_connection
 from wheelchair_speech_recognition_thread import speech_recognition_thread
